2e18e1e5362dc884d7cf412899a43bc42b901534
Avital-Attiah <avitala5515@gmail.com>
2024-11-02 22:42:51 +0200

    Intial Code + Welcome Function

---
+++ stage0/Program4980.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcom4980();
+        Welcome5607();
+    }
+    static partial void Welcome5607();
+    private static void Welcom4980()
+    {
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+    }
+}
